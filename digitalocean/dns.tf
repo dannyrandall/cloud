3c46183a51cff9517e7be116fa1a5a271459d416
Daniel Randall <dgrandall@me.com>
2020-05-19 10:02:33 -0600

    create cert

--- digitalocean/dns.tf
+++ digitalocean/dns.tf
@@ -1,3 +1,9 @@
 data "digitalocean_domain" "personal" {
   name = "danielrandall.dev"
 }
+
+resource "digitalocean_certificate" "cert" {
+  name    = "cert-personal"
+  type    = "lets_encrypt"
+  domains = [data.digitalocean_domain.personal.name]
+}
